be120819cd8f779d3f48069fbbd00e3e78fd246e
Eloizi Nogueira da Silva <eloizi.silva@novamerica.com.br>
2023-09-04 14:00:57 -0300

    testando git pull

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos - curso de Git e GitHub Alura")
 console.log("Aplicação está se conectando ao banco de dados")
+console.log("Testando git pull")
